979825ab7260d112759f489cfcded0af25f022b2
Ubuntu <ubuntu@ip-172-31-92-84.ec2.internal>
2021-12-02 04:53:43 +0000

    added 1 file

---
+++ area_per_circle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created by: Billy Terimpundu 
+# Created on: nov. 27, 2021
+# This program calculates and displays the area and perimeter of a 
+# circle with radius 60 cm.
+import math
+ 
+ 
+def main():
+  print("For a circle that has a radius")
+  print("of 60 cm:")
+  print("")
+  print("Area = {} cm^2". format (math.pi*60**2))
+  print("Perimeter = {} cm". format (2*math.pi*60))
+
+if __name__ == "__main__":
+  main()
